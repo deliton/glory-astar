0e0fbdbf0a46b61ad36b934b037130de3764fe94
deliton <deliton.m@outlook.com>
2020-08-17 14:22:18 -0300

    Updates

---
+++ precache-manifest.d931beed16b7961d2580f2b5e1fec80b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479c3a82b92a9843cc94f80d68a94e96",
+    "url": "/glory-astar/index.html"
+  },
+  {
+    "revision": "bc9aa51c0f2c0149aa33",
+    "url": "/glory-astar/static/css/main.d21156a6.chunk.css"
+  },
+  {
+    "revision": "e7012623399019ad2891",
+    "url": "/glory-astar/static/js/2.660cd9f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/glory-astar/static/js/2.660cd9f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9aa51c0f2c0149aa33",
+    "url": "/glory-astar/static/js/main.9cf08219.chunk.js"
+  },
+  {
+    "revision": "0272a6af90d71ec07444",
+    "url": "/glory-astar/static/js/runtime-main.72b628e8.js"
+  }
+]);
